8bfca47405436d221ddf1e9e7853a3553aa5a0b3
Zafor Iqbal <xoy4444@gmail.com>
2024-08-11 13:29:20 +0600

    Before deploy

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const compression =require('compression')
 const morgan = require("morgan");
 
 module.exports = (app) => {
   app.use(express.json());
+  app.use(compression())
   app.use(cors());
 
   if (process.env.NODE_ENV === "development") {
